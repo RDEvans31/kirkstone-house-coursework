cf3d0822add3b108e71b43e304a2a25c2ed4cbea
RDEvans31 <33652707+RDEvans31@users.noreply.github.com>
2018-08-17 16:30:32 +0800

    Added comments to the form and the php script

--- kirkstone-coursework/addsubjectscript.php
+++ kirkstone-coursework/addsubjectscript.php
@@ -3,12 +3,14 @@
 </head>
 <body>
 <?php
+
 include_once("connection.php");
-array_map("htmlspecialchars", $_POST);
+array_map("htmlspecialchars", $_POST); //this is here to ensure that if SQL should be typed into the input field, it will not affect the database.
+//the line below assigns the contents of the second bracket to the fields stated in the first
 $stmt=$conn->prepare("INSERT INTO tblsubject (Subjectid,Subjectname,Content) VALUES (null,:subject,:content)");
-$stmt->bindParam(':subject', $_POST["subjectname"]);
+$stmt->bindParam(':subject', $_POST["subjectname"]); //assigns the input from the form to the values that will be added to the field
 $stmt->bindParam(':content', $_POST["subjectcontents"]);
-$stmt->execute();
+$stmt->execute(); //executes the SQL with said contents
 $conn=null;
 echo("Data transfer successful")
 
