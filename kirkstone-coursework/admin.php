ba9d4837746401dabab2b768866006f5271c62eb
RDEvans31 <33652707+RDEvans31@users.noreply.github.com>
2018-08-23 21:27:35 +0800

    admin page v5, collapse not working

--- kirkstone-coursework/admin.php
+++ kirkstone-coursework/admin.php
@@ -10,17 +10,6 @@
   </style>
 </head>
 <body>
-  <script>
-  function showform(formID) { //each button is assigned a form id to use with this function.
-    var form=document.getElementById(formID);//locates the relevant form
-    if (form.style.display=="none"){
-      form.style.display="block";//this will toggle the appearence of the form if it is not visible
-  } else {
-      form.style.display="none";
-  }
-    console.log("Success");
-  }
-  </script>
 <?php include_once("connection.php"); ?>
 <nav class="navbar">
     <a class="navbar-brand" href="http://www.kirkstonehouseschool.co.uk/page/default.asp">KHS</a>
@@ -28,78 +17,87 @@
 <div class="jumbotron text-center">
   <h1>Welcome *insert user in here*</h1>
 <!--these divs simply seperate the forms-->
-  <div id="subjectforms">
+  <div id="subjectforms" class="row">
     <p class="pull-left">Subjects:</p>
-    <button class="btn btn-info pull-right" onclick="showform('addingsubject')">Add</button>
-    <button class="btn btn-info pull-right" onclick="showform('teachersubject')">Assign teacher</button>
-    <button class="btn btn-info pull-right" onclick="showform('pupilsubject')">Assign pupil</button><br>
-    <form id="addingsubject" action="addsubjectscript.php" method="post" style="display:none;">
-      <!--This form is for adding a new subject to tblsubject, it sends the these variables to addsubjectscript.php-->
-      Subject:<input type="text" name="subjectname"><br>
-      Content:<input type="text" name="subjectcontents"><br>
-      <input class="btn" type="Submit" value="Add">
-    </form>
-    <form id="teachersubject" action="teachersubjectscript.php" method="post" style="display:none;">
-      <!--this essentially assigns a teacher to a subject using the userid and subjectid keys-->
-      Teacher:<select name="userid">
-        <option value="null">Select a teacher</option>
-        <?php
-        $stmt=$conn->prepare("SELECT * FROM tblusers WHERE privilege=1");
-        $stmt->execute(); //this selects all record in tblusers that have privilege 1, meaning they are a teacher
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-          echo('<option value='.$row["Userid"].'>'.$row["Firstname"].' '.$row["Surname"].'</option>');//this prints them as options
-        }
-        ?>
-      </select><br>
-      <br>
-      Subject:<select name="subjectid">
-        <option value="null">Select a subject</option>
-        <?php
-        $stmt=$conn->prepare("SELECT * FROM tblsubject");
-        $stmt->execute(); //this selects all record in tblsubject
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-          echo('<option value='.$row["Subjectid"].'>'.$row["Subjectname"].'</option>');//this prints them as options
-        }
-        ?>
-      </select><br>
-      <input class="btn" type="Submit" value="Assign">
-    </form>
-    <form id="pupilsubject" action="pupilsubjectscript.php" method="post" style="display:none;">
+    <button type="button" class="btn btn-info pull-right" data-toggle="collapse" data-target="#addingsubject">Add</button>
+    <button type="button" class="btn btn-info pull-right" data-toggle="collapse" data-target="#teachersubject">Assign teacher</button>
+    <button type="button" class="btn btn-info pull-right" data-toggle="collapse" data-target="#pupilsubject">Assign pupil</button><br>
+    <div id="addingsubject" class="collapse">
+      <form action="addsubjectscript.php" method="post">
+        <!--This form is for adding a new subject to tblsubject, it sends the these variables to addsubjectscript.php-->
+        Subject:<input type="text" name="subjectname"><br>
+        Content:<input type="text" name="subjectcontents"><br>
+        <input class="btn" type="Submit" value="Add">
+      </form>
+    </div>
 
-      Pupil:<select name="pupilid">
-        <option value="null">Select a pupil</option>
-        <?php
-        $stmt=$conn->prepare("SELECT * FROM tblpupil");
-        $stmt->execute(); //this selects all record in tblpupil
+    <div id="teachersubject" class="collapse">
+      <form action="teachersubjectscript.php" method="post">
+        <!--this essentially assigns a teacher to a subject using the userid and subjectid keys-->
+        Teacher:<select name="userid">
+          <option value="null">Select a teacher</option>
+          <?php
+          $stmt=$conn->prepare("SELECT * FROM tblusers WHERE privilege=1");
+          $stmt->execute(); //this selects all record in tblusers that have privilege 1, meaning they are a teacher
+          while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+          {
+            echo('<option value='.$row["Userid"].'>'.$row["Firstname"].' '.$row["Surname"].'</option>');//this prints them as options
+          }
+          ?>
+        </select><br>
+        <br>
+        Subject:<select name="subjectid">
+          <option value="null">Select a subject</option>
+          <?php
+          $stmt=$conn->prepare("SELECT * FROM tblsubject");
+          $stmt->execute(); //this selects all record in tblsubject
+          while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+          {
+            echo('<option value='.$row["Subjectid"].'>'.$row["Subjectname"].'</option>');//this prints them as options
+          }
+          ?>
+        </select><br>
+        <input class="btn" type="Submit" value="Assign">
+      </form>
+    </div>
 
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-          echo('<option value='.$row["Pupilid"].'>'.$row["Firstname"].' '.$row["Surname"].'</option>');//this prints them as options
-        }
-        ?>
-      </select><br>
-      <br>
-      Subject:<select name="subjectid">
-        <option value="null">Select a subject</option>
-        <?php
-        $stmt=$conn->prepare("SELECT * FROM tblsubject");
-        $stmt->execute(); //this selects all record in tblsubject
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-          echo('<option value='.$row["Subjectid"].'>'.$row["Subjectname"].'</option>');//this prints them as options
-        }
-        ?>
-      </select><br>
-      <input class="btn" type="Submit" value="Assign">
+    <div id="pupilsubject" class="collapse">
+      <form action="pupilsubjectscript.php" method="post">
+
+        Pupil:<select name="pupilid">
+          <option value="null">Select a pupil</option>
+          <?php
+          $stmt=$conn->prepare("SELECT * FROM tblpupil");
+          $stmt->execute(); //this selects all record in tblpupil
+
+          while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+          {
+            echo('<option value='.$row["Pupilid"].'>'.$row["Firstname"].' '.$row["Surname"].'</option>');//this prints them as options
+          }
+          ?>
+        </select><br>
+        <br>
+        Subject:<select name="subjectid">
+          <option value="null">Select a subject</option>
+          <?php
+          $stmt=$conn->prepare("SELECT * FROM tblsubject");
+          $stmt->execute(); //this selects all record in tblsubject
+          while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+          {
+            echo('<option value='.$row["Subjectid"].'>'.$row["Subjectname"].'</option>');//this prints them as options
+          }
+          ?>
+        </select><br>
+        <input class="btn" type="Submit" value="Assign">
     </form>
+    </div>
   </div>
   <br>
-  <div name="users">
+  <div id="userforms" class="row">
     <p class="pull-left">Users:</p>
-    <button class="btn btn-info pull-right" onclick="showform('addinguser')">Add</button>
-    <form id="addinguser" action="adduserscript.php" method="post" style="display:none;">
+    <button type="button" class="btn btn-info pull-right" data-toggle="collapse" data-target="#addinguser">Add</button>
+    <div id="addinguser" class="collapse">
+    <form action="adduserscript.php" method="post">
       <!--This is for adding a new user to tbluser -->
       First name:<input type="text" name="firstname"><br>
       Surname: <input type="text" name="surname"><br>
@@ -109,11 +107,13 @@
       <input class="btn" type="Submit" value="Add">
     </form>
   </div>
+  </div>
   <br>
-  <div name="pupil">
+  <div id="pupilforms" class="row">
     <p class="pull-left">Pupils:</p>
-    <button class="btn btn-info pull-right" onclick="showform('addingpupil')">Add</button><br>
-    <form id="addingpupil" action="addpupilscript.php" method="post" style="display:none;">
+    <button type="button" class="btn btn-info pull-right" data-toggle="collapse" data-target="#addingpupil">Add</button><br>
+    <div id="addingpupil" class="collapse">
+    <form action="addpupilscript.php" method="post">
       <!-- this is for adding a new pupil to tblpupil-->
       First name:<input type="text" name="pupilfirstname"><br>
       Surname: <input type="text" name="pupilsurname"><br>
@@ -128,12 +128,14 @@
       Score:<input type="text" name="MScore"><br>
       <input class="btn" type="submit" value="Add"><br>
     </form>
+    </div>
   </div>
   <br>
-  <div name="tutorgroup">
+  <div id="tutorgroupforms">
     <p class="pull-left">Tutor groups:</p>
-    <button class="btn btn-info pull-right" onclick="showform('tutorgroup')">Assign</button><br>
-    <form id="tutorgroup" action="tutorgroupscript.php" method="post" style="display:none;">
+    <button type="button" class="btn btn-info pull-right" onclick="showform('tutorgroup')">Assign</button><br>
+    <div id="tutorgroup" class="collapse">
+    <form action="tutorgroupscript.php" method="post">
       Tutor group:<input type="text" name="tutorgroupid"><br>
       Tutor:<select name="userid">
         <option value="null">Select a tutor</option>
@@ -159,6 +161,7 @@
       </select><br>
       <input class="btn" type="Submit" value="Assign">
     </form>
+    </div>
   </div>
 </div>
 </body>
